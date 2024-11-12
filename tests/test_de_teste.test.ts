59dd26d56dabfe621764527fafb593c36b4b27cf
Anthony Silva <anthony.eldyn@gmail.com>
2024-11-12 12:30:59 -0300

    adiiconando testes automatizados e teste de teste

---
+++ tests/test_de_teste.test.ts
@@ -0,0 +1,8 @@
+import { Add } from "../models/add";
+
+describe("Teste de teste", () => {
+  it("Deve ser um teste de teste", () => {
+    const result = Add(1, 1);
+    expect(result).toBe(2);
+  });
+});
