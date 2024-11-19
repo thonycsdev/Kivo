a55a1bd57968dd35bcf07320d5285ac9ef88b1b7
Anthony Silva <anthony.eldyn@gmail.com>
2024-11-19 17:02:03 -0300

    feat: add /status page

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,14 +1,17 @@
-//import UrlManager from '../../utils/urlManager';
-//import { ResponseData } from '../api/v1/status/route';
+import UrlManager from '../../utils/urlManager';
+import { ResponseData } from '../api/v1/status/route';
 
 export default async function Status() {
-	//const urlManager = UrlManager.create();
-	//urlManager.addPathName('/api/v1/status');
-	//const response = await fetch(urlManager.getUrlObject());
-	//const data = (await response.json()) as ResponseData;
+	const urlManager = UrlManager.create();
+	urlManager.addPathName('/api/v1/status');
+	const response = await fetch(urlManager.getUrlObject());
+	const data = (await response.json()) as ResponseData;
 	return (
 		<div>
 			<h1>Status</h1>
+			<p>{data.message}</p>
+			<p>database: {JSON.stringify(data.database)}</p>
+			<p>{data.created_at.toString()}</p>
 		</div>
 	);
 }
