2cecc0d85881485d487e603996103e47b033617e
Anthony Coutinho <anthony.eldyn@gmail.com>
2024-12-09 18:24:31 -0300

    feat: add SignUp functionallity

--- models/user.ts
+++ models/user.ts
@@ -1,6 +1,8 @@
 import { Prisma, User } from '@prisma/client';
 import prisma from 'infra/database';
 import authentication from './authentication';
+import { Credential } from 'types/credential';
+import { ErrorHandler } from 'utils/errorHandler';
 
 async function createUser(user: Prisma.UserCreateInput): Promise<User> {
 	user.password = await authentication.hashPassword(user.password);
@@ -10,6 +12,25 @@ async function createUser(user: Prisma.UserCreateInput): Promise<User> {
 	return userAdded;
 }
 
+async function SignIn(credentials: Credential): Promise<User> {
+	const user = await prisma.user.findUniqueOrThrow({
+		where: {
+			email: credentials.email
+		}
+	});
+	const isSame = await authentication.compare(
+		credentials.password,
+		user.password
+	);
+	if (!isSame) {
+		const error = ErrorHandler.create(new Error('Invalid Password'), 401);
+		error.addSolution('Check your password');
+		throw error;
+	}
+	return user;
+}
+
 export default Object.freeze({
-	createUser
+	createUser,
+	SignIn
 });
--- tests/integration/api/v1/user/user.test.ts
+++ tests/integration/api/v1/user/user.test.ts
@@ -26,5 +26,59 @@ describe('User', () => {
 			const userCreated = await result.json();
 			expect(userCreated.password).not.toBe(user.password);
 		});
+
+		test('Sign Up', async () => {
+			const user = createFakeUserRequest();
+			const result = await fetch('http://localhost:3000/api/v1/user', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(user)
+			});
+			const userCreated = await result.json();
+
+			const resultLogin = await fetch(
+				'http://localhost:3000/api/v1/user/signIn',
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: user.email,
+						password: user.password
+					})
+				}
+			);
+
+			const userLogged = await resultLogin.json();
+			expect(userLogged).toEqual(userCreated);
+		});
+		test('Invalid Sign Up Request', async () => {
+			const user = createFakeUserRequest();
+			await fetch('http://localhost:3000/api/v1/user', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(user)
+			});
+
+			const resultLogin = await fetch(
+				'http://localhost:3000/api/v1/user/signIn',
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: user.email,
+						password: 'invalidPassword'
+					})
+				}
+			);
+			expect(resultLogin.status).toBe(401);
+		});
 	});
 });
