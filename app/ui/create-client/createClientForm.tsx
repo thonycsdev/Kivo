e4b7734904e9a2219f3bc686fa5f88d0bd63e0ae
Anthony Coutinho <anthony.eldyn@gmail.com>
2024-12-17 12:07:28 -0300

    feat: added react-hook-form

--- app/ui/create-client/createClientForm.tsx
+++ app/ui/create-client/createClientForm.tsx
@@ -10,10 +10,16 @@ import {
 	Typography
 } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
+import { Prisma } from '@prisma/client';
+import { Controller, useForm } from 'react-hook-form';
 
 export default function CreateClientForm() {
+	const { register, handleSubmit, control } = useForm<Prisma.ClienteCreateInput>();
+	const handleOnSubmit = (data: Prisma.ClienteCreateInput) => {
+		console.log(data);
+	}
 	return (
-		<>
+		<form onSubmit={handleSubmit(handleOnSubmit)}>
 			<Container
 				sx={{
 					display: 'flex',
@@ -30,52 +36,61 @@ export default function CreateClientForm() {
 				</Box>
 				<Grid2 container columns={4} spacing={4}>
 					<Grid2 size={3}>
-						<TextField fullWidth required label="Nome Completo" />
+						<TextField {...register('name')} fullWidth required label="Nome Completo" />
 					</Grid2>
-					<TextField required label="CPF" />
+					<TextField required label="CPF" {...register('cpf')} />
 					<Grid2 size={2}>
-						<TextField fullWidth required label="Endereço" />
+						<TextField fullWidth required label="Endereço" {...register('address')} />
 					</Grid2>
 					<Grid2 size={2}>
-						<TextField fullWidth required label="Email" />
+						<TextField fullWidth required label="Email" {...register('email')} />
 					</Grid2>
-					<TextField label="Número de Telefone" />
-					<TextField required label="Número de Telefone Pessoal" />
-					<TextField label="Facebook" />
-					<TextField label="Instagram" />
-					<TextField label="Whatsapp" />
-					<TextField required label="Estado Civil" />
+					<TextField label="Número de Telefone" {...register('phoneNumber')} />
+					<TextField required label="Número de Telefone Pessoal" {...register('personalPhoneNumber')} />
+					<TextField label="Facebook" {...register('facebook')} />
+					<TextField label="Instagram" {...register('instagram')} />
+					<TextField label="Whatsapp" {...register('whatsapp')} />
+					<TextField required label="Estado Civil" {...register('maritalStatus')} />
 					<Grid2 container size={4} rowSpacing={4}>
-						<TextField required label="Renda Liquida" />
-						<TextField required label="Renda Bruta" />
+						<TextField required label="Renda Liquida" {...register('netIncome')} />
+						<TextField required label="Renda Bruta" {...register('grossIncome')} />
 						<Grid2>
-							<DatePicker label="Data de Nascimento" />
+							<Controller
+								control={control}
+								name="birthDate"
+								render={({
+									field: { onChange },
+								}) => (
+									<DatePicker onChange={onChange} label="Data de Nascimento" />
+								)}
+							/>
 						</Grid2>
 						<TextField
 							required
 							type="number"
 							defaultValue={1}
 							label="Quantidade de Membros na Família"
+							{...register('familyMembersAmount')}
 						/>
-						<TextField required label="Ocupação" />
-						<TextField label="Cargo" />
+						<TextField required label="Ocupação"{...register('jobTitle')} />
+						<TextField label="Cargo" {...register('jobPosition')} />
 						<FormControlLabel
-							required
 							control={<Switch />}
+							{...register('hasFGTS')}
 							label="Possui FGTS"
 						/>
 						<FormControlLabel
-							required
+							{...register('hasFinancing')}
 							control={<Switch />}
 							label="Possui Algum Financiamento"
 						/>
 					</Grid2>
 					<Grid2 size={3}>
-						<TextField fullWidth required multiline label="Descrição" />
+						<TextField {...register('description')} fullWidth required multiline label="Descrição" />
 					</Grid2>
 				</Grid2>
 				<Box sx={{ alignSelf: 'end', display: 'flex', gap: 3 }}>
-					<Button variant="contained" sx={{ width: '200px' }}>
+					<Button variant="contained" sx={{ width: '200px' }} type='submit'>
 						Cadastrar
 					</Button>
 					<Button variant="outlined" sx={{ width: '200px' }}>
@@ -83,6 +98,6 @@ export default function CreateClientForm() {
 					</Button>
 				</Box>
 			</Container>
-		</>
+		</form>
 	);
 }
