9b25b3783bff90c29b2b661e8eecac56c42f6372
Anthony <thonycout@gmail.com>
2025-05-19 20:58:32 -0300

    fix: rename endpoint string to the new one

--- constants/keys.ts
+++ constants/keys.ts
@@ -1,6 +1,6 @@
 const keys = {
 	status: '/api/v1/status',
-	signIn: '/api/v1/user/signIn',
+	signIn: '/api/v1/user/authentication',
 	signUp: '/api/v1/user/signUp',
 	client: {
 		all: '/api/v1/cliente/all'
